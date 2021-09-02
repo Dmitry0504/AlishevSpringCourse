448f3b2cc549ebfd0cce6dfd3aa8768934d0a049
”Junior” <rip05-04-1990@yandex.ru>
2021-09-02 16:59:21 +0300

    lesson 21, CRUD REST DAO, starting new project

---
+++ springCRUD/src/main/java/application/controllers/PeopleController.java
@@ -0,0 +1,9 @@
+package application.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+}
