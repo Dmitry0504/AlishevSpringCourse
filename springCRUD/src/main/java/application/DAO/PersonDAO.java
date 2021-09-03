e0af29db1fdf30b9abfb2af69b9c8ba2525ed4e3
”Junior” <rip05-04-1990@yandex.ru>
2021-09-03 21:05:17 +0300

    lesson 21, CRUD REST DAO, chapter2

---
+++ springCRUD/src/main/java/application/DAO/PersonDAO.java
@@ -0,0 +1,29 @@
+package application.DAO;
+
+import application.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+
+        people.add(new Person(1, "Tom"));
+        people.add(new Person(2, "John"));
+        people.add(new Person(3, "Eddy"));
+        people.add(new Person(4, "Zak"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
